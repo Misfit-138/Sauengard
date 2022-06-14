136271ed8401625ddc6aec1bc0f8a4986ec0a0a4
Misfit-138 <105970436+Misfit-138@users.noreply.github.com>
2022-06-14 08:10:05 -0400

    update.

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+"""The global keyword is the tool that Python provides for you to opt out of encapsulation
+and break the natural scope of a variable. Encapsulation means that each of your components is a logical,
+self-contained unit that should work as a black box and performs one thing
+(note: this one thing is conceptual and may consist of many, possibly non-trivial, sub-steps)
+without mutating global state or producing side effects.
+The reason is modularity: if something goes wrong in a program (and it will), having strong encapsulation makes it very
+ easy to determine where the failing component is.
+
+Encapsulsation makes code easier to refactor, maintain and expand upon.
+If you need a component to behave differently, it should be easy to remove it or adjust it without these modifications
+ causing a domino effect of changes across other components in the system.
+
+Basic tools for enforcing encapsulation include classes, functions, parameters and the return keyword.
+Languages often provide modules, namespaces and closures to similar effect, but the end goal is always to limit scope
+ and allow the programmer to create loosely-coupled abstractions.
+
+Functions take in input through parameters and produce output through return values.
+You can assign the return value to variables in the calling scope. You can think of parameters as "knobs" that adjust
+ the function's behavior. Inside the function, variables are just temporary storage used by the function needed to
+  generate its one return value then disappear.
+
+Ideally, functions are written to be pure and idempotent; that is, they don't modify global state and produce
+the same result when called multiple times. Python is a little less strict about this than other languages and it's
+natural to use certain in-place functions like sort and random.shuffle. These are exceptions that prove the rule
+ (and if you know a bit about sorting and shuffling, they make sense in these contexts due to the algorithms used
+ and the need for efficiency).
+
+An in-place algorithm is impure and non-idempotent, but if the state that it modifies is limited to its parameter(s)
+and its documentation and return value (usually None) support this, the behavior is predictable and comprehensible."""
 from player_class_module import Player
 import random
 # player_stats = [18,17,16,15,14]
@@ -15,6 +44,6 @@ print(player_1.intelligence)
 print(player_1.wisdom)
 print(player_1.strength)
 print(player_1.dexterity)
-if player_1.dexterity > 12:
-    print("Great dexterity!!")
-player_1.swing()
+#if player_1.dexterity > 12:
+#    print("Great dexterity!!")
+player_1.swing(player_1.dexterity, player_1.strength)
--- player_class_module.py
+++ player_class_module.py
@@ -1,3 +1,4 @@
+import random
 class Player:
 
     def __init__(self,name,constitution,intelligence,wisdom,strength,dexterity):
@@ -11,8 +12,21 @@ class Player:
     def navigate(self):
         print("The player is navigating")
 
-    def swing(self):
-        print(f"{self.name} swings and gets a random swing score added to his dexterity")
+    def swing(self, dexterity, strength):
+        player_roll20 = random.randint(1,20)
+        print(f"Player rolls {player_roll20}")
+        dexterity_modifier = (dexterity - 10) / 2
+        print(f"Dexterity modifier {dexterity_modifier}")
+        monster_roll20 = random.randint(1,20)
+        print(f"Monster rolls {monster_roll20}")
+        if player_roll20 + dexterity_modifier > monster_roll20:
+            player_roll12 = random.randint(1,12)
+            strength_modifier = (strength - 10) / 2
+            damage_to_monster = player_roll12 + strength_modifier
+            print (f"Player hits for {dexterity_modifier} points of damage!")
+        else:
+            print(f"Player misses..")
+
 
     def heals(self):
         print(f"{self.name} takes a restorative tincture")
